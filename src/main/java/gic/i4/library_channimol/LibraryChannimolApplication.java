7aba29904436dc400566e3d6c6144bab19ed9bc4
Nimol <nimol@Nimols-Macbook-pro.local>
2025-05-09 11:03:25 +0700

    midtermDevOps

---
+++ src/main/java/gic/i4/library_channimol/LibraryChannimolApplication.java
@@ -0,0 +1,13 @@
+package gic.i4.library_channimol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryChannimolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryChannimolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gic/i4/library_channimol/LibraryChannimolApplicationTests.java
@@ -0,0 +1,13 @@
+package gic.i4.library_channimol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryChannimolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
